d25c66e575172ffeed64e68b7d42bcdba67418b8
Allan Farca <allan.farca@gmail.com>
2020-05-23 03:04:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount)
    FROM projects
-   INNER JOIN pledges.id
+   INNER JOIN pledges
    ON project_id = pledges.project_id
    JOIN pledges.id ON pledges.project_id = projects.id
    GROUP BY(projects.title);"
