c61d00457199fa876a567fb441d17389233959cf
Allan Farca <allan.farca@gmail.com>
2020-05-23 02:36:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
     ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
     ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
     ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
@@ -11,14 +11,14 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
     ("SQL", "programming", 1000, "5/22/20", "5/22/21");
 
 INSERT INTO pledges (amount, user_id, project_id) VALUES
-    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), 
-    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), 
-    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), 
-    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), 
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1),
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1),
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1),
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1),
     (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1);
 
-INSERT INTO users (name, age) VALUES 
+INSERT INTO users (name, age) VALUES
     ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20),
-    ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20),  
-    ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20),   
+    ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20),
+    ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20),
     ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20);
