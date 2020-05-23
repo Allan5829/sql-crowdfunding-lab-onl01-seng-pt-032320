ad9d086cf528edc9dae4fbd5947e667d8a92c6bc
Allan Farca <allan.farca@gmail.com>
2020-05-23 03:54:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
-   FROM projects
+   FROM projects, pledges
    WHERE projects.category = 'music';"
 end
 
