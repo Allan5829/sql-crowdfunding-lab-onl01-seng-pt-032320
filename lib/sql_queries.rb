7bd8aa60689a72a00f55c7ce6d5a4880b3aa58a4
Allan Farca <allan.farca@gmail.com>
2020-05-23 03:20:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, SUM(pledges.amount)
-   FROM pledges
+   FROM projects
    GROUP BY projects
    HAVING SUM(pledges.amount) > funding_goal;"
 end
