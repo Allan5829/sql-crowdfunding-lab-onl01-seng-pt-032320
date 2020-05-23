db8f7984ab62a1072465522ac2caf61cecf5cdc1
Allan Farca <allan.farca@gmail.com>
2020-05-23 02:36:18 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21"),
+    ("SQL", "programming", 1000, "5/22/20", "5/22/21");
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), 
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), 
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), 
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), 
+    (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1), (10, 1, 1);
+
+INSERT INTO users (name, age) VALUES 
+    ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20),
+    ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20),  
+    ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20),   
+    ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20), ("Allan", 20);
